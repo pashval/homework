35d5fbd4d26979cd7b9fce618dda2bd071007c78
pashval <pashkevychval@gmail.com>
2022-09-16 19:56:00 +0300

    Exam1

---
+++ exam1/assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+$(document).ready(function(){
+    $("a").click(function(){
+        $('.header__burger, .header__nav').removeClass('active');
+        $('body').removeClass('lock');
+    }); 
+}); 
