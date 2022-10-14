1196728e51fe58bf759d8c82c3db371464727b08
pashval <pashkevychval@gmail.com>
2022-10-14 09:09:06 +0300

    commit

--- hw24/assets/js/script.js
+++ hw24/assets/js/script.js
@@ -5,7 +5,7 @@ key = "cf6c42d4";
 
 let getMovie = () => {
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?apikey=${key}&s=${movieName}&page=2`;
+    let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieName}`;
     if(movieName.length <= 0){
         result.innerHTML = "<h3 class='msg'>Please enter</h3>"
     }
@@ -13,17 +13,24 @@ let getMovie = () => {
         fetch(url)
         .then((resp) => resp.json())
         .then((data) => {
-            console.log(data);
-            innerHTML(data);
-            if(data.resp == 'True'){
-                result.innerHTML += `
-                <h2>${data.Title}</h2>`;
-            }
-            else {
-                result.innerHTML=`<h3 class='msg'>${data.Error}</h3>`
-            }
-            
-        })
+            if (data.Response == "True") {
+                result.innerHTML = `
+                  <div class="info">
+                      <img src=${data.Poster} class="poster">
+                      <div>
+                          <h2>${data.Title}</h2>
+                          <div class="genre">
+                              <div>${data.Genre.split(",").join("</div><div>")}</div>
+                          </div>
+                      </div>
+                  </div>
+              `;
+              }
+              //If movie does NOT exists in database
+              else {
+                result.innerHTML = `<h3 class='msg'>${data.Error}</h3>`;
+              }
+            })
         .catch(() => {
             result.innerHTML=`<h3 class='msg'>Error Occured</h3>`
         });
