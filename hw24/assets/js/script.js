8839133461f699fa5ec3756b544488763fe483a0
pashval <pashkevychval@gmail.com>
2022-10-17 14:04:32 +0300

    commit

--- hw24/assets/js/script.js
+++ hw24/assets/js/script.js
@@ -1,41 +1,40 @@
-let movieNameRef = document.getElementById("movie-name");
-let searchBtn = document.getElementById("search-btn");
-let result = document.getElementById("result");
-key = "cf6c42d4";
-
-let getMovie = () => {
-    let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieName}`;
-    if(movieName.length <= 0){
-        result.innerHTML = "<h3 class='msg'>Please enter</h3>"
-    }
-    else {
-        fetch(url)
-        .then((resp) => resp.json())
-        .then((data) => {
-            if (data.Response == "True") {
-                result.innerHTML = `
-                  <div class="info">
-                      <img src=${data.Poster} class="poster">
-                      <div>
-                          <h2>${data.Title}</h2>
-                          <div class="genre">
-                              <div>${data.Genre.split(",").join("</div><div>")}</div>
-                          </div>
-                      </div>
-                  </div>
-              `;
-              }
-              //If movie does NOT exists in database
-              else {
-                result.innerHTML = `<h3 class='msg'>${data.Error}</h3>`;
-              }
-            })
-        .catch(() => {
-            result.innerHTML=`<h3 class='msg'>Error Occured</h3>`
+$(document).ready(function() {
+    $('#search-btn').click(function() {
+        let movieName = $('#movie-name').val();
+        let searchBtn = document.getElementById("search-btn");
+        let choise = $(".choise:checked").val();
+        let url = 'http://www.omdbapi.com/';
+        apiKey = "cf6c42d4";
+        $.ajax(url, {
+            type: 'GET',
+            data: {
+                s: movieName,
+                i: '',
+                apikey: apiKey,
+                type: choise
+            },
+            success: function (data, status, xhr) {
+                innerMovies (data);
+            },
+            error: function (jqXhr, textStatus, errorMessage) {
+                console.log(errorMessage)
+            }
         });
-    }
-};
-
-searchBtn.addEventListener("click", getMovie)
-window.addEventListener("load", getMovie);
+        function innerMovies (data) {
+            let items = data.Search;
+            let newTag = document.createElement('div');
+            let main  = document.querySelector('main');
+            document.body.append(newTag);
+                items.forEach(function (value) {
+                newTag.insertAdjacentHTML(
+                    'afterend',
+                    `<div class="items">
+                        <div class="year">${value.Year}</div>
+                        <div class="title">${value.Title}</div>
+                        <img class="poster" src="${value.Poster}"</img>
+                        </div>`
+                );
+            });
+        };
+    });
+});​
